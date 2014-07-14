9c4d914dad62990f275ce2fe358a2c0b1a973c5a
Marslo <marslo.jiao@gmail.com>
2014-07-14 17:10:00 +0800

    Add CMN

---
+++ CMN/ChoiceMN.py
@@ -0,0 +1,17 @@
+# =============================================================================
+#    FileName: ChoiceMN.py
+#        Desc: 
+#      Author: Marslo
+#       Email: marslo.jiao@gmail.com
+#
+#     Created: 2014-07-01 19:51:03
+#     Version: 0.0.1
+#  LastChange: 2014-07-01 19:51:03
+#     History:
+#              0.0.1 | Marslo | init
+# =============================================================================
+
+import random
+
+clist = random.sample(range(100), 10)
+print clist
